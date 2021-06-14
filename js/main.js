19b8c73a33d2040777b9f9deb9c1960f8d3a6af8
masha8709 <masha.8709@mail.ru>
2021-06-14 23:04:56 +0300

    Создание массивов из 10 объектов

--- js/main.js
+++ js/main.js
@@ -17,3 +17,84 @@ function getRandomFloat(number1, number2, digits = 1) {
 }
 
 getRandomFloat(11.87665, 101.454545, 3);
+
+
+const accomodation = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const timeCheck = ['12:00', '13:00', '14:00'];
+const facilities = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const arrayOfFhoto = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
+
+const getAvatar = function () {
+  let counter = 0;
+  let avatar = '';
+
+  for (let i = 1; i <= 10; i++) {
+    counter++;
+
+    if (counter < 10) {
+      counter = '0' + counter;
+    }
+    avatar = `img/avatars/user${counter}.png`;
+  }
+  return avatar;
+};
+
+
+//даёт одно значение из массива
+const getRandomItem = function (array) {
+  const rand = Math.floor(Math.random() * array.length);
+  return array[rand];
+};
+
+//массив случайной длины из значений, значения не повторяются
+function getListElements(array, counter) {
+  const newArray = [];
+  for (let i = 0; i < counter; i++) {
+    const newElemIndex = getRandomIntInclusive(0, array.length - 1);
+    const newElem = array[newElemIndex];
+    let repeatElements = [];
+    repeatElements = newArray.filter((item) => {
+      if (item === newElem) {
+        return item = newElem;
+      }
+    });
+    const thisElemExist = repeatElements.length;
+
+    if (!thisElemExist) {
+      newArray.push(newElem);
+    }
+  }
+  return newArray;
+}
+
+const author = {
+  avatar: getAvatar(),
+};
+
+const offer = {
+  title: 'Очень хорошее предложение',
+  address: '', //Я так  и додумалась, как мне можно получить строку из lat  и lng, которые в location
+  price: getRandomIntInclusive(0, 1000000),
+  type: getRandomItem(accomodation),
+  rooms: getRandomIntInclusive(1, 100),
+  guests: getRandomIntInclusive(1, 10),
+  checkin: getRandomItem(timeCheck),
+  checkOut: getRandomItem(timeCheck),
+  features: getListElements(facilities, 2),
+  description: 'Очень красивое помещение',
+  photos: getListElements(arrayOfFhoto, 1),
+  location: {
+    lat: getRandomFloat(35.65000, 35.70000),
+    lng: getRandomFloat(139.70000, 139.80000),
+  },
+};
+
+const arrayOfAvatars = [];
+for (let i = 1; i <= 10; i++) {
+  arrayOfAvatars.push(author);
+}
+
+const arrayOfObj = [];
+for (let i = 1; i <= 10; i++) {
+  arrayOfObj.push(offer);
+}
